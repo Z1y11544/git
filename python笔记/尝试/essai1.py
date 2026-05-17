98389c7c67803be5e117842686a6dd9a771f0070
HE Ziyang <hzy090117@gmail.com>
2026-05-17 15:25:05 +0200

    复习fonction de tri par insertion并测试

--- python笔记/尝试/essai1.py
+++ python笔记/尝试/essai1.py
@@ -6,3 +6,16 @@ def selection(lst):
                 min_index = j
         lst[i], lst[min_index] = lst[min_index], lst[i]
     return lst
+def fonct_inser(lst):
+    for i in range(1,len(lst)):
+        en_cours = lst[i]
+        j = i
+        while j > 0 and lst[j-1] > en_cours:
+            lst[j] = lst[j-1]
+            j -= 1
+        lst[j] = en_cours
+    return lst
+
+lst = [17,9,20,14,5,3,0,7]
+print(selection(lst))
+print(fonct_inser(lst))

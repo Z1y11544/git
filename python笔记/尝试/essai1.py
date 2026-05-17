af0f4078576432aace95ec100497a7cf9419c2c7
HE Ziyang <hzy090117@gmail.com>
2026-05-17 15:33:08 +0200

    复习二分查找并测试

--- python笔记/尝试/essai1.py
+++ python笔记/尝试/essai1.py
@@ -17,5 +17,22 @@ def fonct_inser(lst):
     return lst
 
 lst = [17,9,20,14,5,3,0,7]
+lst_tri = selection(lst)
 print(selection(lst))
 print(fonct_inser(lst))
+
+def cherche_dichotomie(lst,x):
+    debut = 0
+    fin = len(lst)-1
+    mid = (debut + fin) // 2
+    while debut <= fin:
+        if lst[mid] == x:
+            return True
+        elif lst[mid] < x:
+            debut = mid + 1
+        else:
+            fin = mid - 1
+        mid = (debut + fin) // 2
+    return False
+
+print(cherche_dichotomie(lst_tri, 7))
